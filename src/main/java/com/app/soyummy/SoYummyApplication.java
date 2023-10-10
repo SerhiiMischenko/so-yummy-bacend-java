39e39797682ef4a311f5680757524053acc70d0f
SerhiiMischenko <serj.roshen@gmail.com>
2023-10-10 11:53:34 +0300

    pilot

---
+++ src/main/java/com/app/soyummy/SoYummyApplication.java
@@ -0,0 +1,13 @@
+package com.app.soyummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoYummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoYummyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/soyummy/SoYummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.soyummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoYummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
